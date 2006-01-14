29e566a8cd88900741dab22c9f0f1efef4ec833c
arnee <arnee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-01-14 17:56:26 +0000

    Fixed "boubled-content" bug with WP2 Added xmlns to enable validation
    git-svn-id: https://plugins.svn.wordpress.org/google-sitemap-generator/trunk@4654 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sitemap.php
+++ sitemap.php
@@ -112,11 +112,11 @@
  ==============================================================================
  Copyright 2005  ARNE BRACHHOLD  (email : himself [a|t] arnebrachhold [dot] de)
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -124,7 +124,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  
  
  Developer Documentation
@@ -831,9 +831,9 @@ class GoogleSitemapGenerator {
 	var $_content = array();
 
 	/**
-	 * @var bool Saves if the generator is registered to the shutdown function
-	 */	
-	var $_registerd = false;
+	 * @var int The last handled post ID
+	 */		
+	var $_lastPostID = 0;
 	
 	/**
 	 * Returns the path to the blog directory
@@ -1168,15 +1168,11 @@ class GoogleSitemapGenerator {
 	 * @access public
 	 * @author Arne Brachhold <himself [at] arnebrachhold [dot] de>
 	*/
-	function CheckForAutoBuild($postID = 0) {
-		if($postID>0) {
-			$this->Initate();
-			if($this->GetOption("b_auto_enabled")===true) {
-				if(!$this->_registered) {
-					add_action("shutdown",array(&$this, 'BuildSitemap'),99,0);
-					$this->_registered = true;
-				}
-			}
+	function CheckForAutoBuild($postID) {
+		$this->Initate();
+		if($this->GetOption("b_auto_enabled")===true && $this->_lastPostID != $postID) {
+			$this->_lastPostID = $postID;
+			$this->BuildSitemap();	
 		}
 	}
 	
@@ -1538,7 +1534,7 @@ class GoogleSitemapGenerator {
 		}
 		
 		//Go XML!
-		$this->AddElement(new GoogleSitemapGeneratorXmlEntry('<urlset xmlns="http://www.google.com/schemas/sitemap/0.84">'));
+		$this->AddElement(new GoogleSitemapGeneratorXmlEntry('<urlset xmlns="http://www.google.com/schemas/sitemap/0.84" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 http://www.google.com/schemas/sitemap/0.84/sitemap.xsd">'));
 		
 		//Add the home page (WITH a slash!)
 		if($this->GetOption("in_home")) {
